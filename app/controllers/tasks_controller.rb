e9585d523ed102635366b0604e446242b73046be
栗田大輔 <daikons.msn36@gmail.com>
2021-09-12 00:48:00 +0900

    controllers/tasks_controller.rbでcreate,updateアクションの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,10 +6,28 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Property was successfully created."
+    else
+      render :new
+    end
+  end
   def edit
+  end
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Property was successfully updated."
+    else
+      render :edit
+    end
   end
   private
   def set_task
     @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:task_name, :task_detail)
   end
 end
