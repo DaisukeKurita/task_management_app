f08a90222b21395c02984cb66ee0db9d8b876bdb
栗田大輔 <daikons.msn36@gmail.com>
2021-09-11 23:04:33 +0900

    views/tasks/_form.html.erbを作成しnew、editが表示されるように実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ edit ]
   def index
     @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def edit
+  end
+  private
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
