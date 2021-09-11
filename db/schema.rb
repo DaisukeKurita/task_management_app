d7280c274fffa4e638e9156ff1adfdf48a9c2b99
栗田大輔 <daikons.msn36@gmail.com>
2021-09-11 17:51:25 +0900

    tasksテーブルのtask_nameにデータベース制約（not_nullを設定）

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_051223) do
+ActiveRecord::Schema.define(version: 2021_09_11_083015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
+    t.string "task_name", null: false
     t.text "task_detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
