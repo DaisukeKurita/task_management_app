29905a586b3bd8bb1ecb47d628e71928c0f64b19
栗田大輔 <daikons.msn36@gmail.com>
2021-09-13 23:13:04 +0900

    rspec spec/models/task_spec.rbを実装、:task_detailにpresence: trueも設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :task_name, presence: true, length: { maximum: 15 }
-  validates :task_detail, length: { maximum: 255 }
+  validates :task_detail, presence: true, length: { maximum: 255 }
 end
