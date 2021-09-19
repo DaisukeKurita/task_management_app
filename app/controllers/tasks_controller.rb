9d92d2e441df7a71ca29ae10c87776a533697e4c
栗田大輔 <daikons.msn36@gmail.com>
2021-09-20 01:56:04 +0900

    views/tasks/index.html.erbで終了期限でソートできるように実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
   def index
-    @tasks = Task.all.order(created_at: :desc)
+    if params[:sort_expired]
+      @tasks = Task.all.order(expired_at: :desc)
+    else
+      @tasks = Task.all.order(created_at: :desc)
+    end
   end
   def new
     @task = Task.new
