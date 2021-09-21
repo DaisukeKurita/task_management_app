47aecb078dab3566d725bb7c9bd28637a9e32432
栗田大輔 <daikons.msn36@gmail.com>
2021-09-21 23:25:06 +0900

    tasksテーブルのstatusカラムにpresence: trueのバリデーションを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :task_name, presence: true, length: { maximum: 15 }
   validates :task_detail, presence: true, length: { maximum: 255 }
+  validates :status, presence: true
 end
