f1ab35a53a18ea0f0a48d29c6c6b0df639b31477
栗田大輔 <daikons.msn36@gmail.com>
2021-09-25 23:31:40 +0900

    System Specの修正と検索機能のテストを実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,9 +3,13 @@ describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:task) }
   let!(:second_task) { FactoryBot.create(:second_task) }
   let!(:third_task) { FactoryBot.create(:third_task) }
+  let!(:four_task) { FactoryBot.create(:four_task) }
+  let!(:five_task) { FactoryBot.create(:five_task) }
+
   before do
     visit tasks_path
   end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -13,9 +17,11 @@ describe 'タスク管理機能', type: :system do
         fill_in 'タスク名', with: 'test_task_name'
         fill_in 'タスク詳細', with: 'test_task_detail'
         fill_in '終了期限', with: Time.new
+        select '完了'
       end
     end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
@@ -25,17 +31,18 @@ describe 'タスク管理機能', type: :system do
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
         task_list = all('table tr td')[0]
-        expect(task_list).to have_content 'third'
+        expect(task_list).to have_content 'name_five'
       end
     end
     context '終了期限でソートするというリンクを押した場合' do
       it '終了期限の降順に並び替えられたタスクが一番上に表示される' do
         visit tasks_path(sort_expired: "true")
         task_list = all('table tr td')[0]
-        expect(task_list).to have_content 'second'
+        expect(task_list).to have_content 'name_four'
       end
     end
   end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
@@ -43,5 +50,29 @@ describe 'タスク管理機能', type: :system do
          expect(page).to have_content 'name'
        end
      end
+  end
+
+  describe '検索機能' do
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in '検索', with: 'four'
+        click_button 'Search'
+        expect(page).to have_content 'name_four'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        select '着手中'
+        click_button 'Search'
+        expect(page).to have_content 'name_second'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        fill_in '検索', with: 'five'
+        click_button 'Search'
+        expect(page).to have_content 'name_five'
+      end
+    end
   end
 end
