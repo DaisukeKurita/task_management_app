84da925555eb7ac8806f1e7206d1063211a40739
栗田大輔 <daikons.msn36@gmail.com>
2021-09-25 13:27:49 +0900

    task_nameにindexを貼った

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+
   def index
     if params[:sort_expired]
       @tasks = Task.all.order(expired_at: :desc)
@@ -17,9 +18,11 @@ class TasksController < ApplicationController
       @tasks = Task.all.order(created_at: :desc)
     end
   end
+
   def new
     @task = Task.new
   end
+
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -28,10 +31,13 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
   def show
   end
+
   def edit
   end
+
   def update
     if @task.update(task_params)
       redirect_to tasks_path, notice: t('notice.Task was successfully updated')
@@ -39,10 +45,12 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: t('notice.Task was successfully destroyed')
   end
+
   private
   def set_task
     @task = Task.find(params[:id])
