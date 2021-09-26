da35241e29dc348c7103ae42c3cf96cfcf467271
栗田大輔 <daikons.msn36@gmail.com>
2021-09-27 00:30:26 +0900

    Taskモデルにpriorityカラムを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :task_name, presence: true, length: { maximum: 15 }
   validates :task_detail, presence: true, length: { maximum: 255 }
   validates :status, presence: true
+  validates :priority, presence: true
   enum status: {
     not_started_yet: 0,
     under_start: 1,
