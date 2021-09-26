b212629c2ace33235211112cf93067968650e179
栗田大輔 <daikons.msn36@gmail.com>
2021-09-27 01:09:54 +0900

    priority（優先順位）の機能が使用できるように各viewに実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,11 +3,18 @@ class Task < ApplicationRecord
   validates :task_detail, presence: true, length: { maximum: 255 }
   validates :status, presence: true
   validates :priority, presence: true
+
   enum status: {
     not_started_yet: 0,
     under_start: 1,
     completion: 2
     }
+  enum priority: {
+    high: 0,
+    medium: 1,
+    low: 2
+    }
+
     scope :creation_date_descending, -> {order(created_at: :desc)}
     scope :end_deadline_descending, -> {order(expired_at: :desc)}
     scope :search_task_name, ->(params) { where('task_name LIKE ?', "%#{params}%")}
