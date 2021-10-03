2b39fefd9c8a972318998b89551e23a83508ef21
栗田大輔 <daikons.msn36@gmail.com>
2021-10-03 22:57:41 +0900

    ログイン時にアクセスできないページを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,21 @@ class SessionsController < ApplicationController
   skip_before_action :login_required, only: %i[ new create ]
 
   def new
+    redirect_to tasks_path if logged_in?
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+    if logged_in?
+      redirect_to tasks_path
     else
-      flash.now[:danger] = t('notice.Failed to login')
-      render :new
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user.id)
+      else
+        flash.now[:danger] = t('notice.Failed to login')
+        render :new
+      end
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,30 @@ class UsersController < ApplicationController
   skip_before_action :login_required, only: %i[ new create ]
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to tasks_path
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
+    if logged_in?
+      redirect_to tasks_path
+    else
+      @user = User.new(user_params)
       if @user.save
           session[:user_id] = @user.id
           redirect_to user_path(@user.id), notice: t('notice.User was successfully created')
       else
         render :new
       end
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to tasks_path unless @user.id == current_user.id
   end
 
   private
