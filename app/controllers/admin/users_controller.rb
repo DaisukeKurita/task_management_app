161fc32cac23d4c1e8f8077b72a3cce6bb63e062
栗田大輔 <daikons.msn36@gmail.com>
2021-10-06 16:00:46 +0900

    ユーザーの管理者機能を実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,56 @@
+class Admin::UsersController < ApplicationController
+  skip_before_action :login_required, only: %i[ new create ]
+  before_action :set_user, only: %i[ edit update show destroy ]
+  before_action :back_when_logged_in, only: %i[ new create ]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to admin_user_path(@user.id), notice: t('notice.User was successfully created')
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id), notice: t('notice.User was successfully updated')
+    else
+      render :edit
+    end
+  end
+
+  def show
+    redirect_to tasks_path unless @user.id == current_user.id
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: t('notice.User was successfully destroyed')
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def back_when_logged_in
+    redirect_to tasks_path if logged_in?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  resources :users, only: [:new, :create, :show, :index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :index, :destroy]
+  namespace :admin do
+    resources :users
+  end
 end
