d838a3cd933bec0ed0870bb8151f0037c228eb91
栗田大輔 <daikons.msn36@gmail.com>
2021-10-06 15:59:29 +0900

    if logged_in?の所をback_when_logged_inメソッドとして共通化処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,23 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: %i[ new create ]
   before_action :set_user, only: %i[ show destroy ]
+  before_action :back_when_logged_in, only: %i[ new create ]
+
+  def index
+    @users = User.all
+  end
 
   def new
-    if logged_in?
-      redirect_to tasks_path
-    else
-      @user = User.new
-    end
+    @user = User.new
   end
 
   def create
-    if logged_in?
-      redirect_to tasks_path
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: t('notice.User was successfully created')
     else
-      @user = User.new(user_params)
-      if @user.save
-          session[:user_id] = @user.id
-          redirect_to user_path(@user.id), notice: t('notice.User was successfully created')
-      else
-        render :new
-      end
+      render :new
     end
   end
 
@@ -28,14 +25,6 @@ class UsersController < ApplicationController
     redirect_to tasks_path unless @user.id == current_user.id
   end
 
-  def index
-    @users = User.all
-  end
-
-  def destroy
-    @user.destroy
-    redirect_to  new_session_path
-  end
 
   private
   def set_user
@@ -43,6 +32,10 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:user_name, :email, :password,:password_confirmation)
+    params.require(:user).permit(:user_name, :email, :admin, :password,:password_confirmation)
+  end
+
+  def back_when_logged_in
+    redirect_to tasks_path if logged_in?
   end
 end
