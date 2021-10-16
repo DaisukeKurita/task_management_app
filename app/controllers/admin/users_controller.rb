2a1d0ed82847727c7a24a2f25775c907d4574cd9
栗田大輔 <daikons.msn36@gmail.com>
2021-10-16 22:42:56 +0900

    ユーザー作成時のログイン処理を削除

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,7 +14,6 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       redirect_to admin_user_path(@user.id), notice: t('notice.User was successfully created', name: @user.user_name)
     else
       render :new
