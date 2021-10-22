d5a1d4bde55cdf718b086d4260fb28bbe7f54b68
栗田大輔 <daikons.msn36@gmail.com>
2021-10-22 21:23:13 +0900

    ラベル検索機能の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,33 +6,22 @@ class TasksController < ApplicationController
     @tasks = current_user.tasks.end_deadline_descending.page(params[:page]).per(5) if params[:sort_expired] 
     @tasks = current_user.tasks.highest_priority.page(params[:page]).per(5) if params[:sort_priority]
     if params[:task].present?
-      if params[:task][:search].present? && params[:task][:status].present? && params[:task][:label_id].present?
-        @tasks = current_user.tasks.search_task_name(params[:task][:search]).search_status(params[:task][:status]).label_search(params[:task][:label_id]).page(params[:page]).per(5)
-      elsif params[:task][:search].present?
+      if params[:task][:search].present? && params[:task][:status].present? && params[:task][:label_id].present? # タスク名、ステータス、ラベル検索
+        @tasks = current_user.tasks.search_task_name(params[:task][:search]).search_status(params[:task][:status]).joins(:labels).search_label(params[:task][:label_id]).page(params[:page]).per(5)
+      elsif params[:task][:search].present? && params[:task][:status].present? # タスク名、ステータス検索
+        @tasks = current_user.tasks.search_task_name(params[:task][:search]).search_status(params[:task][:status]).page(params[:page]).per(5)
+      elsif params[:task][:search].present? && params[:task][:label_id].present? # タスク名、ラベル検索
+        @tasks = current_user.tasks.search_task_name(params[:task][:search]).joins(:labels).search_label(params[:task][:label_id]).page(params[:page]).per(5)
+      elsif params[:task][:status].present? && params[:task][:label_id].present? # ステータス、ラベル検索
+        @tasks = current_user.tasks.search_status(params[:task][:status]).joins(:labels).search_label(params[:task][:label_id]).page(params[:page]).per(5)
+      elsif params[:task][:search].present? # タスク名検索
         @tasks = current_user.tasks.search_task_name(params[:task][:search]).page(params[:page]).per(5)
-      elsif params[:task][:status].present?
+      elsif params[:task][:status].present? # ステータス検索
         @tasks = current_user.tasks.search_status(params[:task][:status]).page(params[:page]).per(5)
-      elsif params[:task][:label_id].present?
+      elsif params[:task][:label_id].present? # ラベル検索
         @tasks = current_user.tasks.joins(:labels).search_label(params[:task][:label_id]).page(params[:page]).per(5)
       end
     end
-    # if params[:sort_expired]
-    #   @tasks = current_user.tasks.end_deadline_descending.page(params[:page]).per(5)
-    # elsif params[:sort_priority]
-    #   @tasks = current_user.tasks.highest_priority.page(params[:page]).per(5)
-    # elsif params[:task].present?
-    #   if params[:task][:search].present? && params[:task][:status].present?
-    #     @tasks = current_user.tasks.search_task_name(params[:task][:search]).search_status(params[:task][:status]).page(params[:page]).per(5)
-    #   elsif params[:task][:search].present?
-    #     @tasks = current_user.tasks.search_task_name(params[:task][:search]).page(params[:page]).per(5)
-    #   elsif params[:task][:status].present?
-    #     @tasks = current_user.tasks.search_status(params[:task][:status]).page(params[:page]).per(5)
-    #   else
-    #     @tasks = current_user.tasks.creation_date_descending.page(params[:page]).per(5)
-    #   end
-    # else
-    #   @tasks = current_user.tasks.creation_date_descending.page(params[:page]).per(5)
-    # end
   end
 
   def new
