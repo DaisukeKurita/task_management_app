5f9de5ea33f0c1633cb81d056a985091a7892fc7
栗田大輔 <daikons.msn36@gmail.com>
2021-10-23 18:23:39 +0900

    label_nameに制約とバリデーションを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_150319) do
+ActiveRecord::Schema.define(version: 2021_10_23_091506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_10_20_150319) do
   end
 
   create_table "labels", force: :cascade do |t|
-    t.string "label_name"
+    t.string "label_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
